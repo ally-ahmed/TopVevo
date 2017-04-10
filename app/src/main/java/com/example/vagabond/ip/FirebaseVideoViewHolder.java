01442cfc8790a807e947d4b8c7b984d1682fefa9
vagabond <norbixin@live.com>
2017-04-10 17:34:37 +0300

    added save and firebase and authentication

---
+++ app/src/main/java/com/example/vagabond/ip/FirebaseVideoViewHolder.java
@@ -0,0 +1,8 @@
+package com.example.vagabond.ip;
+
+/**
+ * Created by vagabond on 4/8/17.
+ */
+
+public class FirebaseVideoViewHolder {
+}
---
+++ app/src/main/java/com/example/vagabond/ip/SavedVideoActivity.java
@@ -0,0 +1,12 @@
+package com.example.vagabond.ip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
